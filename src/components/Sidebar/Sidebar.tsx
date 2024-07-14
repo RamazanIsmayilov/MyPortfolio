7f7a13e68344af281ebb03d9f6d498b21ac82f56
unknown <ismayiloff522@gmail.com>
2024-07-14 16:09:54 +0400

    home page

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -8,13 +8,17 @@ import { GrProjects } from "react-icons/gr";
 import { IoFingerPrint } from "react-icons/io5";
 import { GoPaperclip } from "react-icons/go";
 import { PiSwatches } from "react-icons/pi";
+import { FaInstagram } from "react-icons/fa";
+import { CiFacebook } from "react-icons/ci";
+import { CiLinkedin } from "react-icons/ci";
+import { FaGithub } from "react-icons/fa";
 
 const Sidebar: React.FC = () => {
   return (
     <div className='sidebar bg-darkBlue text-white p-7 border border-customRed'>
       <div className="heading flex items-center justify-between gap-20">
         <div className='flex items-center gap-3'>
-          <div className='icon px-3 py-4 bg-brightBlue'>
+          <div className='logo px-3 py-4 bg-brightBlue'>
             <LogoIcon />
           </div>
           <h6 className='text-2xl text-gray'>Rİ</h6>
@@ -23,7 +27,7 @@ const Sidebar: React.FC = () => {
           <h6 className='text-2xl text-gray'>{new Date().getHours()}:{new Date().getMinutes()} PM</h6>
         </div>
       </div>
-      <div className="me flex gap-5 mt-5 p-5">
+      <div className="me flex gap-5 mt-6 p-5">
         <div className="img ">
           <img width={65} height={90} src={MeIMg} alt="" />
         </div>
@@ -37,30 +41,33 @@ const Sidebar: React.FC = () => {
         </div>
 
       </div>
-      <div className="menu mt-16">
+      <div className="menu mt-12">
         <nav>
-          <ul className='flex flex-col gap-6'>
+          <ul className='flex flex-col gap-3'>
             <li>
-              <NavLink to='/' className='flex items-center gap-4 text-gray'><PiHouse className='menu-icon text-xl' /> Home</NavLink>
+              <NavLink to='/' className='flex items-center gap-4 text-gray hover:text-white focus:text-white p-3'><PiHouse className='menu-icon text-xl text-blue' /> Home</NavLink>
             </li>
             <li>
-              <NavLink to='/about' className='flex items-center gap-4 text-gray'><IoFingerPrint className='menu-icon' /> About</NavLink>
+              <NavLink to='/about' className='flex items-center gap-4 text-gray hover:text-white focus:text-white p-3'><IoFingerPrint className='menu-icon text-blue' /> About</NavLink>
             </li>
             <li>
-              <NavLink to='/projects' className='flex items-center gap-4 text-gray'><GrProjects className='menu-icon' /> Projects</NavLink>
+              <NavLink to='/contact' className='flex items-center gap-4 text-gray hover:text-white focus:text-white p-3'><GoPaperclip className='menu-icon text-blue' /> Contact</NavLink>
             </li>
             <li>
-              <NavLink to='/contact' className='flex items-center gap-4 text-gray'><GoPaperclip className='menu-icon' /> Contact</NavLink>
+              <NavLink to='/skills' className='flex items-center gap-4 text-gray hover:text-white focus:text-white p-3'><PiSwatches className='menu-icon text-blue' /> Skills</NavLink>
             </li>
             <li>
-              <NavLink to='/skills' className='flex items-center gap-4 text-gray'><PiSwatches className='menu-icon' /> Skills</NavLink>
+              <NavLink to='/projects' className='flex items-center gap-4 text-gray hover:text-white focus:text-white p-3'><GrProjects className='menu-icon text-blue' /> Projects</NavLink>
             </li>
           </ul>
         </nav>
       </div>
       <div className="footer flex justify-between mt-16">
-        <div className="left flex">
-
+        <div className="left flex gap-7 items-center px-3 py-2">
+          <NavLink to='https://www.instagram.com/r.ismayilof' target='blank'><FaInstagram /></NavLink>
+          <NavLink to='https://www.facebook.com/profile.php?id=61562651615021&mibextid=ZbWKwL' target='blank'><CiFacebook /></NavLink>
+          <NavLink to='https://www.linkedin.com/in/ramazanismay%C4%B1lov' target='blank'><CiLinkedin /></NavLink>
+          <NavLink to='https://github.com/RamazanIsmayilov' target='blank'><FaGithub /></NavLink>
         </div>
         <div className="right px-3 py-2">
           <span className='text-gray'>© {new Date().getFullYear()}</span>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import MainImg from '../../assets/images/main.png'
+import { FaRegClock } from "react-icons/fa";
 
-const Home = () => {
+const Home: React.FC = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className="hero bg-darkBlue flex items-center gap-5 p-5">
+        <div className="hero-img">
+          <img width={220} src={MainImg} alt="image/main" />
+        </div>
+        <div className="hero-content">
+          <div className="top flex items-center gap-5 p-3">
+            <div className="available flex items-center gap-5 text-white px-3 py-2" style={{ fontSize: '14px' }}>
+              <span className='online-dot'></span>
+              Available for freelance work
+            </div>
+            <div className="time flex items-center text-white gap-3 px-5 py-2">
+              <FaRegClock className='text-blue' />
+              {new Date().getMonth()}
+              -
+              {new Date().getDate()}
+              -
+              {new Date().getFullYear()}
+            </div>
+            <div className="contact-me"></div>
+          </div>
+          <div className="bottom"></div>
+        </div>
+      </div>
+    </div>
   )
 }
 
